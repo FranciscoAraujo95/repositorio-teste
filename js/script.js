c98169397659d5498e4b0825e0c577165d3ab364
FranciscoAraujo95 <fhydglan@gmail.com>
2024-07-01 09:57:13 -0300

    new button with new function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-const btn = document.querySelector(".btn").addEventListener("click", () => {
+const btn1 = document.querySelector(".btn1").addEventListener("click", () => {
   document.body.style.backgroundImage =
     "linear-gradient(to right, purple, blue)";
   alert("Você clicou!!!");
+});
+
+const btn2 = document.querySelector(".btn2").addEventListener("click", () => {
+  let n1 = Number(prompt("Digite um número: "));
+  let n2 = Number(prompt("Digite outro número: "));
+  let n3 = Number(prompt("Digite outro número: "));
+
+  let media = (n1 + n2 + n3) / 3;
+
+  alert(`Média: ${media.toFixed(2)}.`);
 });
