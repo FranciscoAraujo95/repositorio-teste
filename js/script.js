3ab2eeee91c8913d0702bc4f39d28623e82443e8
FranciscoAraujo95 <fhydglan@gmail.com>
2024-07-01 09:50:44 -0300

    added some other files

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector(".btn").addEventListener("click", () => {
+  document.body.style.backgroundImage =
+    "linear-gradient(to right, purple, blue)";
+  alert("Você clicou!!!");
+});
